7a6305e14b78aae786600708c05623c9d37de99e
Sofia Marinich <sophiay16102000@gmail.com>
2024-01-29 21:40:33 +0300

    added comments to conftest.py and README.md

--- conftest.py
+++ conftest.py
@@ -10,11 +10,13 @@ from data_for_tests import constants as const
 
 @pytest.fixture(scope="function")
 def new_bun():
+    """Фикстура создания булки"""
     return Bun(random.choice(const.bun_names)+" булка", random.uniform(const.min_bun_price, const.max_bun_price))
 
 
 @pytest.fixture(scope="function")
 def new_ingredient():
+    """Фикстура создания нового рандомного ингредиента: соуса или начинки"""
     ingredient_type = random.choice([types.INGREDIENT_TYPE_SAUCE, types.INGREDIENT_TYPE_FILLING])
     if ingredient_type == types.INGREDIENT_TYPE_FILLING:
         ingredient_name = random.choice(const.fillings)
@@ -27,6 +29,7 @@ def new_ingredient():
 
 @pytest.fixture(scope="function")
 def new_sauce():
+    """Фикстура создания соуса"""
     ingredient_type = types.INGREDIENT_TYPE_SAUCE
     ingredient_name = random.choice(const.sauces)
     ingredient_price = random.uniform(const.min_sauces_price, const.max_sauces_price)
@@ -35,6 +38,7 @@ def new_sauce():
 
 @pytest.fixture(scope="function")
 def new_filling():
+    """Фикстура создания начинки"""
     ingredient_type = types.INGREDIENT_TYPE_FILLING
     ingredient_name = random.choice(const.fillings)
     ingredient_price = random.uniform(const.min_filling_price, const.max_filling_price)
@@ -43,6 +47,7 @@ def new_filling():
 
 @pytest.fixture(scope="function")
 def new_burger(new_bun, new_filling, new_sauce):
+    """Фикстура создания бургера"""
     burger = Burger()
     burger.set_buns(new_bun)
     burger.add_ingredient(new_filling)
